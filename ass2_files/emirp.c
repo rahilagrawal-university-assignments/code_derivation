7dc1283b9cee12196cfa4d3ab52f5737bb87b5df
rahilagrawal3416 <rahilagrawal3416@gmail.com>
2018-04-21 18:59:32 +1000

    1. Started .tex file. 2. Started emirp.c file. Added required .sty files

---
+++ ass2_files/emirp.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main (int argc, char* argv[]){
+	int n;
+	scanf("%lu", &n);
+	printf("%lu\n",emirp(n));
+}
+unsigned long emirp(unsigned long n){
+	//TODO
+	int r = 13;
+	return r;
+}
+
+void isPrime(int r, int *a){
+	//TODO
+}
