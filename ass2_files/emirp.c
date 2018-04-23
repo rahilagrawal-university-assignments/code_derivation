2718eeedef5bf421c07566e8b64827bf008c7e4a
Aditya <karia.adi1998@yahoo.com>
2018-04-23 18:17:11 +1000

    c implementation written up

--- ass2_files/emirp.c
+++ ass2_files/emirp.c
@@ -1,17 +1,66 @@
 #include <stdio.h>
+#include "reverse.h"
 
+unsigned long emirp(unsigned long n);
+void isPrime(unsigned long r, int *a);
 
 int main (int argc, char* argv[]){
-	int n;
-	scanf("%lu", &n);
+	unsigned long n = 5;
+	// scanf("%lu", &n);
 	printf("%lu\n",emirp(n));
 }
-unsigned long emirp(unsigned long n){
-	//TODO
-	int r = 13;
+
+/*
+var i := 1
+r := 13
+while i != n do
+    r := r + 1
+    var a := 1
+    isPrime(r,a)
+    if a = 1 then 
+        var s := 0
+        reversn(r,s)
+        var b := 1
+        isPrime(s, b)
+        if b = 1 && s != r then
+            i = i + 1
+od
+
+*/
+unsigned long emirp(unsigned long n) {
+	int i = 1;
+	unsigned long r = 13;
+    while (i != n) {
+        r = r + 1;
+        int a = 1;
+        isPrime(r, &a);
+        if (a == 1) {
+            unsigned long s = 0;
+            reversen(r, &s);
+            int b = 1;
+            isPrime(s, &b);
+            if (b == 1 && s != r) {
+                i = i + 1;
+            }
+        }
+    }
 	return r;
 }
 
-void isPrime(int r, int *a){
-	//TODO
+/*
+var j := 0
+while j != r do
+    if r mod j = 0 then
+        a = 0
+    j := j + 1
+od
+*/
+void isPrime(unsigned long r, int *a) {
+    unsigned long j = 2;
+    while (j != r) {
+        if (r % j == 0) {
+            *a = 0;
+        }
+        j = j + 1;
+    }
 }
