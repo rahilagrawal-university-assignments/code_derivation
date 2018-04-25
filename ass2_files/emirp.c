bd14938579b947d4edf2e8d1a999d4221b4eb07f
rahilagrawal3416 <rahilagrawal3416@gmail.com>
2018-04-25 22:37:44 +1000

    Latex progress

--- ass2_files/emirp.c
+++ ass2_files/emirp.c
@@ -10,57 +10,32 @@ int main (int argc, char* argv[]){
 	   printf("%lu\n",emirp(n));
 }
 
-/*
-var i := 1
-r := 13
-while i != n do
-    r := r + 1
-    var a := 1
-    isPrime(r,a)
-    if a = 1 then 
-        var s := 0
-        reversen(r,s)
-        var b := 1
-        isPrime(s, b)
-        if b = 1 && s != r then
-            i = i + 1
-od
-
-*/
 unsigned long emirp(unsigned long n) {
 	int i = 1;
+    unsigned long x = 13;
 	unsigned long r = 13;
     while (i != n) {
-        r = r + 1;
+        x = x + 1;
         int a = 1;
         isPrime(r, &a);
         if (a == 1) {
             unsigned long s = 0;
-            reversen(r, &s);
+            reversen(x, &s);
             int b = 1;
             isPrime(s, &b);
-            if (b == 1 && s != r) {
+            if (b == 1 && s != r)
                 i = i + 1;
-            }
+                r = x;
         }
     }
 	return r;
 }
 
-/*
-var j := 0
-while j != r do
-    if r mod j = 0 then
-        a = 0
-    j := j + 1
-od
-*/
 void isPrime(unsigned long r, int *a) {
     unsigned long j = 2;
     while (j != r) {
-        if (r % j == 0) {
+        if (r % j == 0)
             *a = 0;
-        }
         j = j + 1;
     }
 }
